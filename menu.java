07e31527a1c3badaff91c46cddb9160976ad81b2
Alan Achilles <alanachilles21@gmail.com>
2023-11-14 20:45:08 -0500

    Finsihed first menu
    Centered title and play button

--- menu.java
+++ menu.java
@@ -4,14 +4,44 @@ import java.awt.*;
 public class menu {
   public menu(){
     JFrame jFrame = new JFrame("Monster Hunter");
+    jFrame.setBackground(Color.BLACK);
+    
     JPanel jPanel = new JPanel(); 
+    jPanel.setLayout(new BoxLayout(jPanel, BoxLayout.Y_AXIS));
+    jPanel.setBackground(Color.BLACK);
+    
+    JLabel title = new JLabel("Monster Hunter");
+    title.setForeground(Color.WHITE); // Set text color to white
+    title.setFont(new Font("Verdana", Font.BOLD, 80)); 
+
+    jPanel.add(Box.createRigidArea(new Dimension(0, 300))); //Adds line break
+
+    JButton playButton = new JButton("Play");
+    playButton.setForeground(Color.WHITE); // Set text color to white
+    playButton.setBackground(Color.GRAY); 
+    playButton.setBorderPainted(false);
+    playButton.setFocusPainted(false);
+    playButton.setPreferredSize(new Dimension(200, 50));
+    
+    JPanel playButtonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+    playButtonPanel.setBackground(Color.BLACK);
+    playButtonPanel.add(playButton);
+
+    JPanel titlePanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+    titlePanel.setBackground(Color.BLACK);
+    titlePanel.add(title);
+
+    jPanel.add(titlePanel);
+
+    jPanel.add(Box.createRigidArea(new Dimension(0, 100))); //Adds line break
+    
+    jPanel.add(playButtonPanel);
+
     jFrame.setSize(1500,1000);
     jFrame.setLocationRelativeTo(null);
-    jFrame.setResizable(false);
-    jFrame.setVisible(true);
-    jFrame.setBackground(Color.BLACK);
+    jFrame.setResizable(true);
     jFrame.add(jPanel);
-    jPanel.setBackground(Color.BLACK);
+    jFrame.setVisible(true);
     jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
   }
 }
